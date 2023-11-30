bfec3ad89926f6912763c0655dc8138597f4c4ea
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2023-11-30 20:54:41 +0300

    added clitics rules

--- CustomizedTreebankTokenizer.py
+++ CustomizedTreebankTokenizer.py
@@ -1,14 +1,13 @@
 import re
 class CustomizedTreebankTokenizer():
 
-    #todo: add rule to split don't into do and n't
     #can't is a special case and will be substituted with cannot (expansion)
     #n't: look wether ' is surrounded by n and t and then split before n
     #all other clitcs are simply separated but not expanded
 
     RULES = [
         (re.compile(r"([\(\)\[\]\{\}\<\>])"), r" \1 "),  # pad brackets with whitespaces
-        (re.compile(r"([\"\'\´\`])"), r" \1 "),  # pad quotation marks with whitespaces
+        (re.compile(r"([\"\´\`])"), r" \1 "),  # pad quotation marks with whitespaces
         (re.compile(r"\n"), r" "),  # replace newline by whitespace
         (re.compile(r"([.,;:!?%\"\'\)\]])(?=\s|$|\n)"), r" \1 "),  # separate punctuation that is followed by a whitespace
         (re.compile(r"^([.,;:!?%\"\'])"), r" \1 "),  # separate punctuation at the beginning of a string
@@ -18,19 +17,29 @@ class CustomizedTreebankTokenizer():
     ]
 
     CLITICS_RULES = [
-
+         #regular cases will be split
+        (re.compile(r"(\b\w+)(\'m)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(\'ve)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(\'ll)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(\'d)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(\'s)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(\'re)\b"), r"\1 \2"),
+        (re.compile(r"(\b\w+)(n\'t)\b"), r"\1 \2"),
     ]
 
     def tokenize(self, text: str, splitClitcsEnabled):
-
-        for pattern, substitution in self.RULES:
-            text = pattern.sub(substitution, text)
-        text =  text.split()
 
         if splitClitcsEnabled:
             for pattern, substitution in self.CLITICS_RULES:
                 text = pattern.sub(substitution, text)
 
-        return text.split()
+        text = re.sub(r"\bwon\'t\b", "will not", text)
+        text = re.sub(r"\bcan\'t\b", "cannot", text)
+
+        for pattern, substitution in self.RULES:
+            text = pattern.sub(substitution, text)
+        text = text.split()
+
+        return text
 
 
